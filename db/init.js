561ae83ec6aa21ae637c713d32d5551f9b98b945
scottistas1406 <120509564+scottistas1406@users.noreply.github.com>
2023-05-06 16:48:22 -0500

    Update init.js

--- db/init.js
+++ db/init.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 const client = new Client({
-  connectionString: DATABASE_URL='postgres://wizard_0asp_user:XWyLcbN4dHObsjzw9j6YWjbG6B6llxPp@dpg-chbbv5u7avjcvo5998sg-a.oregon-postgres.render.com/wizard_0asp'
+ connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
